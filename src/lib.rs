7e3ec4ad4a935ca1b399e6f31ad784fedccec54e
ryan <ryan@kurte.nz>
2020-03-30 16:24:28 +1300

    updating for new rpc

--- src/lib.rs
+++ src/lib.rs
@@ -13,20 +13,20 @@ extern crate futures;
 use futures::prelude::*;
 
 extern crate dsf_core;
+use dsf_core::prelude::*;
 use dsf_core::types::DataKind;
-use dsf_core::api::{ServiceHandle, Create as _, Locate as _, Subscribe as _, Publish as _, Register as _};
+use dsf_core::api::ServiceHandle;
 
 extern crate dsf_client;
-use dsf_client::Client;
-pub use dsf_client::Error;
+use dsf_client::prelude::*;
 
 extern crate dsf_rpc;
-use dsf_rpc::{self as rpc, ServiceInfo, PublishInfo, LocateInfo};
+use dsf_rpc::{self as rpc, ServiceIdentifier, ServiceInfo, PublishInfo, LocateInfo};
 
 pub mod endpoint;
 
-pub mod commands;
-pub use commands::*;
+pub mod options;
+pub use options::*;
 
 pub const IOT_APP_ID: u16 = 1;
 
@@ -35,6 +35,7 @@ pub const IOT_DATA_PAGE_KIND: u16 = 2;
 
 
 /// IotClient wraps a `dsf_client::Client` and provides interfaces to interact with DSF-IoT services
+/// TODO: one day this should be an extension trait
 pub struct IotClient {
     client: Client,
 }
@@ -42,14 +43,14 @@ pub struct IotClient {
 
 impl IotClient {
     /// Create a new DSF-IoT client using the provided path
-    pub fn new(path: &str, timeout: Duration) -> Result<Self, Error> {
+    pub fn new(path: &str, timeout: Duration) -> Result<Self, ClientError> {
         let client = Client::new(path, timeout)?;
 
         Ok(Self{client})
     }
 
     /// Create a new IoT service
-    pub async fn create(&mut self, options: CreateOptions) -> Result<ServiceHandle, Error> {
+    pub async fn create(&mut self, options: CreateOptions) -> Result<ServiceHandle, ClientError> {
 
         let req = rpc::service::CreateOptions {
             application_id: IOT_APP_ID,
@@ -66,34 +67,22 @@ impl IotClient {
     }
 
     /// Register an existing service in the database
-    pub async fn register(&mut self, options: RegisterOptions) -> Result<dsf_rpc::service::RegisterInfo, Error> {
-        self.client.register(options).await
+    pub async fn register(&mut self, handle: &ServiceHandle) -> Result<dsf_rpc::service::RegisterInfo, ClientError> {
+        self.client.register(handle).await
     }
 
     /// Publish an existing IoT service
-    pub async fn publish(&mut self, options: PublishOptions) -> Result<PublishInfo, Error> {
-        
-        let req = rpc::data::PublishOptions {
-            service: options.service,
-            kind: Some(DataKind::Unknown(IOT_DATA_PAGE_KIND)),
-            // TODO: encoded body here
-            data: None,
-        };
-
-        self.client.publish(req).await
+    pub async fn publish(&mut self, handle: &ServiceHandle, kind: DataKind, data: &[u8]) -> Result<PublishInfo, ClientError> {
+        self.client.publish(handle, Some(kind), Some(data)).await
     }
 
     /// Search for an existing IoT service
-    pub async fn search(&mut self, options: SearchOptions) -> Result<LocateInfo, Error> {
-        let req = rpc::service::LocateOptions{
-            id: options.id,
-        };
-
-        self.client.locate(req).await
+    pub async fn search(&mut self, id: &Id) -> Result<(ServiceHandle, LocateInfo), ClientError> {
+        self.client.locate(id).await
     }
 
     /// List known IoT services
-    pub async fn list(&mut self, _options: ListOptions) -> Result<Vec<ServiceInfo>, Error> {
+    pub async fn list(&mut self, _options: ListOptions) -> Result<Vec<ServiceInfo>, ClientError> {
         let req = rpc::service::ListOptions {
             application_id: Some(IOT_APP_ID),
         };
@@ -102,18 +91,18 @@ impl IotClient {
     }
 
     /// Query for data from an IoT service
-    pub async fn query(&mut self, _options: QueryOptions) -> Result<ServiceHandle, Error> {
+    pub async fn query(&mut self, _options: QueryOptions) -> Result<ServiceHandle, ClientError> {
         unimplemented!()
     }
 
     /// Subscribe to data from an IoT service
-    pub async fn subscribe(&mut self, options: SubscribeOptions) -> Result<impl Stream<Item=()>, Error> {
+    pub async fn subscribe(&mut self, handle: &ServiceHandle, options: SubscribeOptions) -> Result<impl Stream<Item=()>, ClientError> {
         
         let req = rpc::service::SubscribeOptions {
             service: options.service,
         };
 
-        let resp = self.client.subscribe(req).await?;
+        let resp = self.client.subscribe(handle, req).await?;
 
         // TODO: decode endpoint info here
         Ok(resp.map(|_d| () ))
